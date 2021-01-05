7698e5988a5f9872bf3a1fb36d3b82b951f9d15d
Drew Thompson <drewallynthompson@gmail.com>
2021-01-05 22:04:14 +0000

    Automatically backed up by Learn

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,6 +1,9 @@
 class DoctorsController < ApplicationController
+<<<<<<< HEAD
   
   def show
     @doctor = Doctor.find(params[:id])
   end 
+=======
+>>>>>>> c1e5e833b24b36499e75cb19c9877e2ce3db2b63
 end
--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 class Doctor < ApplicationRecord 
   has_many :appointments
   has_many :patients, through: :appointments
+=======
+class Doctor < ApplicationRecord
+>>>>>>> c1e5e833b24b36499e75cb19c9877e2ce3db2b63
 end
